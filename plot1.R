1cb934071980a2e15d409cfb38b983a920ac86c7
BjarneLiller <jonasjoe2@m7.stud.ku.dk>
2014-08-09 18:48:44 +0200

    try
    ytr

---
+++ plot1.R
@@ -0,0 +1,12 @@
+plot1 <- function() {
+        data <- read.csv("household_power_consumption.txt", skip = 66500, nrows = 4600)
+        head(data)
+        
+        
+        
+        
+        
+        
+        
+        
+}
